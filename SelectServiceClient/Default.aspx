465e7aac087a598e6f9cc265a8a442eca36a0e85
Sophia Allen <smlelevich@gmail.com>
2016-05-05 10:50:48 -0700

    First Commit

---
+++ SelectServiceClient/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Show Tracker Service Client</title>
+    <link rel="stylesheet" href="StyleSheet.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Search For Shows</h1>
+        <h4><em>Search by artist or venue, or display all shows.</em></h4>
+    <div>
+        <label id="artists">Artists:<br /><asp:DropDownList ID="ArtistDropDown" runat="server"></asp:DropDownList></label>
+        <label>Venues:<br /><asp:DropDownList ID="VenueDropDown" runat="server"></asp:DropDownList></label>
+        <label>Shows:<br /><asp:DropDownList ID="ShowDropDown" runat="server"></asp:DropDownList></label>
+        <asp:Button ID="SelectArtist" runat="server" Text="Search By Artist" OnClick="SelectArtist_Click" />
+        <asp:Button ID="SelectVenue" runat="server" Text="Search By Venue" OnClick="SelectVenue_Click" />
+        <asp:Button ID="DisplayShows" runat="server" Text="List All Shows" OnClick="DisplayShows_Click"/>
+        <asp:GridView ID="ShowGridView" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SelectServiceClient/Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerServiceRef.ShowTrackerServiceClient stc = new ShowTrackerServiceRef.ShowTrackerServiceClient();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Fill_Artist_DropDown();
+            Fill_Venue_DropDown();
+            Fill_Show_DropDown();
+        }
+            
+    }
+    protected void Fill_Artist_DropDown()
+    {
+        string[] artists = stc.GetArtists();
+        ArtistDropDown.DataSource = artists;
+        ArtistDropDown.DataBind();
+ 
+    }
+
+    protected void Fill_Venue_DropDown()
+    {
+        string[] venues = stc.GetVenues();
+        VenueDropDown.DataSource = venues;
+        VenueDropDown.DataBind();
+    }
+
+    protected void Fill_Show_DropDown()
+    {
+        string[] shows = stc.GetShows();
+        ShowDropDown.DataSource = shows;
+        ShowDropDown.DataBind();
+    }
+    protected void SelectArtist_Click(object sender, EventArgs e)
+    {
+        string selectedArtist = ArtistDropDown.SelectedItem.Text;
+        ShowTrackerServiceRef.Performance[] shows = stc.GetArtistShows(selectedArtist);
+        ShowGridView.DataSource = shows;
+        ShowGridView.DataBind();
+    }
+
+
+
+    protected void SelectVenue_Click(object sender, EventArgs e)
+    {
+        string selectedVenue = VenueDropDown.SelectedItem.Text;
+        ShowTrackerServiceRef.Performance[] shows = stc.GetVenueShows(selectedVenue);
+        ShowGridView.DataSource = shows;
+        ShowGridView.DataBind();
+    }
+
+    protected void DisplayShows_Click(object sender, EventArgs e)
+    {
+        string[] shows = stc.GetShows();
+        ShowGridView.DataSource = shows;
+        ShowGridView.DataBind();
+    }
+}
